6d4ca8ec6b2fb2ddc9dc644c1bec85107c590c34
Harindu973 <harindulakshankob@gmail.com>
2021-02-23 20:51:44 +0530

    Added folder structure

---
+++ src/pages/login/login.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+//import Row from "../../components/footer/footer"
+//import "./newsletter.css";
+
+class Compo extends Component {
+    render(){
+        return(
+            <div>
+                
+
+            </div>
+        );
+    }
+}
+export default Compo;
